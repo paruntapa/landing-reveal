d07e53f8da63233efd7a7b83ae947910e1efcb66
paruntapa <geniustechmax@gmail.com>
2025-03-10 23:41:12 +0530

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="hero">
+      <div className="progress-bar">
+        <p>loading</p>
+        <p><span id="counter">0</span></p>
+      </div>
+      
+    </div>
+  );
+}
