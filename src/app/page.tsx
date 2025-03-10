d9726966e14f164b27e0011dcf531317681a9967
paruntapa <geniustechmax@gmail.com>
2025-03-11 01:34:25 +0530

    completed core functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,111 @@
-import Image from "next/image";
+"use client"
+import gsap from "gsap";
+import CustomEase from "gsap/CustomEase";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  useEffect(()=> {
+    gsap.registerPlugin(CustomEase);
+
+    const customEase = CustomEase.create("custom", ".87, 0, .13, 1");
+  
+    const counter = document.getElementById("counter");
+  
+    gsap.set(".video-container", {
+      scale: 0,
+      rotation: -20,
+    })
+  
+    gsap.to(".hero", {
+      clipPath: "polygon(0% 45%, 25% 45%, 25% 55%, 0% 55%)",
+      duration: 1.5,
+      ease: customEase,
+      delay: 1,
+    })
+
+    gsap.to(".hero", {
+      clipPath: "polygon(0% 45%, 100% 45%, 100% 55%, 0% 55%)",
+      duration: 2,
+      ease: customEase,
+      delay: 3,
+
+      onStart: () => {
+        gsap.to(".progress-bar", {
+          width: "100vw",
+          duration: 2,
+          ease: customEase,
+        })
+  
+        gsap.to(counter, {
+          innerHTML: 100,
+          duration: 2,
+          ease: customEase,
+          snap: { innerHTML: 1}
+        })
+      }
+    });
+
+    gsap.to(".hero", {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",   
+      duration: 1,
+      ease: customEase,
+      delay: 5,
+      onStart: ()=> {
+        gsap.to(".video-container", {
+          scale: 1,
+          rotation: 0,
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+          duration: 1.25,
+          ease: customEase,
+        })
+        gsap.to(".progress-bar", {
+          opacity: 0,
+          duration: 0.3,
+        })
+
+        gsap.to(".logo", {
+          left: "0%",
+          transform: "translateX(0%)",
+          duration: 1.25,
+          ease: customEase,
+
+          onStart: () => {
+            gsap.to(".char.anim-out h1", {
+              y: "100%",
+              duration: 1,
+              stagger: -0.075,
+              ease: customEase,
+            });
+
+            gsap.to(".char-anim-in h1", {
+              x: "-1200%",
+              duration: 1,
+              ease: customEase,
+              delay: 0.25,
+            })
+          }
+        })
+      }
+    });
+
+    gsap.to([".header span", ".coordinates span"], {
+      y: "0%",
+      duration: 1,
+      stagger: 0.125,
+      ease: "power3.out",
+      delay: 5.75,
+    })
+
+    
+  }, [])
+
+
   return (
     <div className="hero">
       <div className="progress-bar">
         <p>loading</p>
-        <p><span id="counter">0</span></p>
+        <p><span id="counter">/0</span></p>
       </div>
 
       <div className="video-container">
@@ -31,7 +131,7 @@ export default function Home() {
         <h1><span>Combat from</span></h1>
         <h1><span>the streets of</span></h1>
         <h1><span>Dehradun</span></h1>
-        <h1><span>( watch the fight)</span></h1>
+        <p><span>( watch the fight)</span></p>
       </div>
 
       <div className="coordinates">
